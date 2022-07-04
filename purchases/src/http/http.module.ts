19f6fa47745d50b0f741db9c15f5d039e010de14
Wilson Franca <wilson.franca.92@gmail.com>
2022-07-04 13:23:58 -0700

    feat(customers): registered customer resolver

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -9,6 +9,7 @@ import { ProductsService } from 'src/services/products.service';
 import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 import { PurchasesService } from 'src/services/purchases.service';
 import { CustomersService } from 'src/services/customer.service';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { CustomersService } from 'src/services/customer.service';
     // Resolvers
     ProductsResolver,
     PurchasesResolver,
+    CustomersResolver,
 
     // Services
     ProductsService,
