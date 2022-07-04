82e2c6e70487fe404de8a5272855f00d594fbe76
Wilson Franca <wilson.franca.92@gmail.com>
2022-07-04 13:36:02 -0700

    feat(customers): added method to allow querying the purchases in the me query

--- purchases/src/http/graphql/resolvers/customers.resolver.ts
+++ purchases/src/http/graphql/resolvers/customers.resolver.ts
@@ -1,17 +1,26 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
 import { CustomersService } from 'src/services/customer.service';
+import { PurchasesService } from 'src/services/purchases.service';
 import { Customer } from '../models/customer';
 
 @Resolver(() => Customer)
 export class CustomersResolver {
-  constructor(private customersService: CustomersService) {}
+  constructor(
+    private customersService: CustomersService,
+    private purchasesService: PurchasesService,
+  ) {}
 
   @UseGuards(AuthorizationGuard)
   @Query(() => Customer)
   me(@CurrentUser() user: AuthUser) {
     return this.customersService.getCustomerByAuthUserId(user.sub);
+  }
+
+  @ResolveField()
+  purchases(@Parent() customer: Customer) {
+    return this.purchasesService.listAllPurchasesByCustomer(customer.id);
   }
 }
