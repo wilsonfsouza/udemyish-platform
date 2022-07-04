08b8fd6bbd87eccfc361a84c40a26dca25f1fcf7
Wilson Franca <wilson.franca.92@gmail.com>
2022-07-04 13:17:12 -0700

    chore: code lint

--- purchases/src/http/graphql/inputs/ create-product-input.ts
+++ purchases/src/http/graphql/inputs/ create-product-input.ts
@@ -1,7 +1,7 @@
-import { Field, InputType } from "@nestjs/graphql";
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateProductInput {
   @Field()
-  title: string
+  title: string;
 }
--- purchases/src/http/graphql/models/purchase.ts
+++ purchases/src/http/graphql/models/purchase.ts
@@ -1,30 +1,30 @@
-import { Field, ID, ObjectType, registerEnumType } from "@nestjs/graphql";
-import { Product } from "./product";
+import { Field, ID, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { Product } from './product';
 
 enum PurchaseStatus {
   PENDING = 'PENDING',
   APPROVED = 'APPROVED',
-  FAILED = 'FAILED'
+  FAILED = 'FAILED',
 }
 
 registerEnumType(PurchaseStatus, {
   name: 'PurchaseStatus',
-  description: 'Available purchase statuses'
-})
+  description: 'Available purchase statuses',
+});
 
 @ObjectType()
 export class Purchase {
   @Field(() => ID)
-  id: string
+  id: string;
 
   @Field(() => PurchaseStatus)
-  status: PurchaseStatus
+  status: PurchaseStatus;
 
   @Field(() => Date)
-  createdAt: Date
+  createdAt: Date;
 
   @Field(() => Product)
-  product: Product
+  product: Product;
 
-  productId: string
+  productId: string;
 }
